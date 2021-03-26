19958029e01b98771986aec8ac24789c70716c5d
fire <annabeth.phoenix@gmail.com>
2021-03-26 10:46:39 -0500

    added env for port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require("dotenv").config()
 const express = require("express");
-
 const app = express();
-const server = app.listen(3000);
+const server = app.listen(process.env.PORT);
 app.use(express.static("public"));
